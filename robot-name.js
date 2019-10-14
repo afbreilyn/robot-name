0973377940b85ff20a0bf0c58db43a41b6692159
Aaron Foster Breilyn <afbreilyn@stridenyc.com>
2019-10-14 11:23:03 -0400

    Return hardcoded robot name

--- robot-name.js
+++ robot-name.js
@@ -1,6 +1,11 @@
 // This is only a SKELETON file for the 'Robot Name' exercise. It's been
 // provided as a convenience to get your started writing code faster.
 
-export class Robot { }
+export class Robot {
+	get name () {
+		return "AA123"
+	}
+}
+
 
 Robot.releaseNames = () => { };
